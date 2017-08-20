f7004459f57a948c2e1eaf92ab877ac9e27c11f3
User <olig.su@googlemail.com>
2017-08-20 10:15:03 +0500

    3+

--- 3.php
+++ 3.php
@@ -1,9 +1,13 @@
 <?PHP
-$array= ['North America'=>['Mammuthus columbi','Serpentes'],
+$array= ['North America'=>['Mammuthus columbi qwerty','Serpentes'],
 'South America'=>['Mammuthus exilis','Panthera'],
 'Africa'=>['Mammutidae','Crocodilia','Panthera leo'],
 'Eurasia'=>['Ursidae','Canis lupus','Vulpes lagopus'],
 'Australia'=>['Macropus','Tachyglossidae']];
+$array2=[];
+$araay3=[];
+$array4=[];
+$array5=[];
 
 echo '1:'. PHP_EOL;
 print_r($array);
@@ -17,20 +21,20 @@ foreach ($array as $key1 => $value1)
     if (strpos($value2,' ')>0)
     {
      $array2[]=$value2;
+     echo $value2 . PHP_EOL;
+     $array3=explode(" ",$value2);
+     $array4[]=$array3[0];
+     unset($array3[0]);
+     $array5[]=implode(' ',$array3);
     }
   }
 }
 
-foreach ($array2 as $value) {
-  echo $value . PHP_EOL;
-  $array3=explode(" ",$value);
-  $array4[]=$array3[0];
-  $array5[]=$array3[1];
-}
 shuffle($array5);
 
 echo '3:'. PHP_EOL;
 foreach ($array4 as $key=> $value) {
   echo $array4[$key].' '.$array5[$key].PHP_EOL;
 }
+
 ?>
